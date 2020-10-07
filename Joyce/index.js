5a95f27cfa5910a9c77c993ae3e6db6c88e2433d
Joyce <puch1996@gmail.com>
2020-10-07 14:03:34 +0200

    modified

---
+++ Joyce/index.js
@@ -0,0 +1,25 @@
+// 1
+let introSentence = "hello, my name is Anna and I am ";
+let age = 45;
+console.log(introSentence + age);
+
+//  The + operator is concatenating the string and the number
+
+// 2
+let value1 = "1005";
+console.log(parseInt(value1));
+let value2 = "10.05";
+console.log(parseFloat(value2));
+
+// 3
+let isEven1 = 30;
+let checkEven1 = isEven1 == 30 ? "30 is even" : "it is odd";
+console.log(checkEven1);
+
+let isEven2 = 939;
+let checkEven2 = isEven2 == 30 ? "30 is even" : "it is odd";
+console.log(checkEven2);
+
+let isEven3 = 40.9;
+let checkEven3 = isEven3 == 30 ? "30 is even" : "it is odd";
+console.log(checkEven3);
