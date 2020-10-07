f19f1e8b09d90418e34d7fa3457951d7a7fb6d91
carveler <3heyaberlin@gmail.com>
2020-10-07 15:57:34 +0200

    mami

---
+++ mami/script.js
@@ -0,0 +1,17 @@
+let introSentence = "hello, my name is Fran and I am";
+let age = 25;
+
+console.log(introSentence + " " + age);
+
+let str = 1005;
+let fltstr = 10.05;
+console.log(parseInt(str));
+console.log(parseFloat(fltstr));
+
+let str2 = 30;
+console.log(str2 % 2 === 0 ? str2 + " is even" : str2 + " is odd.");
+
+let str3 = 939;
+console.log(str3 % 2 === 0 ? str3 + " is even" : str3 + " is odd.");
+let str4 = 40.9;
+console.log(str4 % 2 === 0 ? str4 + " is even" : str4 + " is odd.");
