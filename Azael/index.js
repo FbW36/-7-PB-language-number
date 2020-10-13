df882c23f593fa3352b5a366972ac765061f52f0
Zarzarius <zarzarius@gmail.com>
2020-10-13 14:40:55 +0200

    correction

--- Azael/index.js
+++ Azael/index.js
@@ -2,23 +2,24 @@
 
 // #### 1. Concatenation.
 /* Create two variables. One variable should contain a string value and the other should contain a number. Concatenate the string and the number.
-```javascript
+```javascript*/
 let introSentence = "hello, my name is Fran and I am";
 let age = 25;
-```
 
-**Expected Output**: 
+console.log(introSentence + " " + age);
+
+/* **Expected Output**: 
 ```javascript
 "hello, my name is Fran and I am 25"
 ```
 * In this case, what is the `+` operator doing? Comment your answer in the js file.
 
 
-// Answer==> To concatenate them 
+// Answer==> To concatenate them */
 
-#### 2. Converting 
-* Create a variable with the value of "1005". Convert it to a number.
-* Create a variable with the value of "10.05". Convert it to a number. */
+/*#### 2. Converting
+ * Create a variable with the value of "1005". Convert it to a number.
+ * Create a variable with the value of "10.05". Convert it to a number. */
 
 let integer = "1005";
 let integerNumber = Number(integer);
